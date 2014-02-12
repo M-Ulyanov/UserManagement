81773f8a70230bb5cd444b08dba471f91382eb28
M.Ulyanov <web.ulyanov@gmail.com>
2014-02-13 00:26:53 +0300

    Version 1

---
+++ js/script.js
@@ -0,0 +1,294 @@
+$(function(){
+
+		//Глобальные переменные
+		var form = $('#add_user_box'),
+			table = $('#table_users_box'),
+			message = $('#message-box'),
+			current = 0,
+			userId = 0,
+			errorMessage  = '<span class="error_message">Ошибка при добавлении пользователя!</span>',
+			errorMessage2  = '<span class="error_message">Ошибка при удаление пользователя!</span>',
+			validDatа = '<span class="valid_data">Пользователь был успешно добавлен!</span>',
+			invalidDatа = '<span class="invalid_data">Ошибка!Пользователь не добавлен!</span>',
+			validMessage  = '<span class="valid_message">Добавлен новый пользователь - </span>',
+			deleteMessage  = '<span class="delete_message">Был удален пользователь - </span>',
+			editMessage  = '<span class="edit_message">Данные пользователей были изменены </span>';
+
+
+		//Блок добавления пользователей
+		$('#add_user_button').on('click', function(event){
+			 event.preventDefault();
+			if($(form).is(':hidden')){
+				$(form).show(500);
+				$(this).removeClass('open-button').addClass('close-button')
+
+			}else{
+				$(form).hide(500);
+				$(this).removeClass('close-button').addClass('open-button');
+			}
+		});
+
+
+		//Проверка формы добавления пользователей
+		$('#new_user_button').on('click', function(){
+			var date = new Date();
+			$('#add_user').find('input').each(function() {
+				if($(this).val() === '' || $(this).val().length < 3){
+					alertInValid()
+					$(this).removeClass('ok').addClass('error');
+				}
+				else if($(this).hasClass('emailAdd')){
+					var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+					if(pattern.test($('#mail').val())){
+						$('#mail').removeClass('error').addClass('ok');
+					}
+					else{
+						alertInValid()
+						$('#mail').removeClass('ok').addClass('error');
+					}
+				}
+				else if($(this).hasClass('loginAdd')){
+					if($('.login').length !== 0){
+						$('#table_users').find('.login').each(function() {
+							var thiss = $(this).val();
+							var a = $('#login').val();
+							if(a === thiss){
+								alertInValid()
+								$('#login').removeClass('ok').addClass('error');						
+								$('#error_login').animate({
+									opacity : 'show'
+								});					
+							}
+							else{	
+								$('#login').removeClass('error').addClass('ok');
+							}						
+						});
+					}
+					else{
+						$('#login').removeClass('error').addClass('ok');
+					}
+				}
+				else{											
+					$(this).removeClass('error').addClass('ok');
+				}				
+			});
+
+			if($('#add_user').find('input').hasClass('error')){
+				current++;
+				$('<div></div>').addClass('message-item').appendTo(message)
+				$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+					+'<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() + '</span>'
+					+ errorMessage);
+				return false;
+			}
+			else{
+				addUser();
+			}
+		});
+
+
+		//Функция вывода ошибки при добавлении
+		function alertInValid(){
+			$('#add_user span').remove();
+			$(invalidDatа).appendTo('#add_user').animate({
+				opacity : 'show',
+			});	
+		}
+
+
+		//Функция Добавление пользователей
+		function addUser(){
+			$('#add_user span').remove();
+			$(validDatа).appendTo('#add_user').animate({
+				opacity : 'show',
+				});	
+			var date = new Date();
+			userId ++;
+			current++;
+			var userName = $('#name').val();
+			var userLogin = $('#login').val();
+			var userMail = $('#mail').val();
+			var userPass = $('#pass').val();
+			var userAccess = $('#access').val();
+			$('<div></div>').addClass('message-item').appendTo(message)
+			$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+				+'<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() + '</span>'
+				+ validMessage + '<b>' + userLogin + '</b>');
+			$('<tr></tr>').addClass('tr' + userId).appendTo('#table_users');
+			$('<td></td>').append($('<span class="id" </span>').text(userId)).appendTo('#table_users tr:last');
+			$('<td></td>').append($('<input class="login" type="text" disabled/>').val(userLogin)).appendTo('#table_users tr:last');
+			$('<td></td>').append($('<input class="name" type="text" disabled/>').val(userName)).appendTo('#table_users tr:last');
+			$('<td></td>').append($('<input class="mail" type="text" disabled/>').val(userMail)).appendTo('#table_users tr:last');
+			$('<td></td>').append($('<input class="pass" type="text" disabled/>').val(userPass)).appendTo('#table_users tr:last');
+			$('<td></td>').append($('<input class="access" type="text" disabled/>').val(userAccess)).appendTo('#table_users tr:last');
+			$('#table_users tr:last').append("<td class='editDate'>"+ '<a class="edit" href="#">EDIT</a>' +"</td>");
+			$('#table_users tr:last').append("<td>"+ '<a class="del" href="#">DEL</a>' +"</td>");
+			$('#table_users tr td:first-child').css('width', '50px')
+		};	
+		
+
+		//Анимация блока удаления
+		$('#del_user_button').on('click', function(event){
+			 event.preventDefault();
+			if($('#delete_user_box').is(':hidden')){
+				$('#delete_user_box').slideDown(300);
+				$(this).removeClass('open-button').addClass('close-button');
+			}
+			else{
+				$('#delete_user_box').slideUp(300);
+				$(this).removeClass('close-button').addClass('open-button');
+			}
+		});
+
+		
+		//Удаление пользователей
+		$('#delete_user_button').on('click', function(){
+			current ++;
+			var date = new Date();
+			var idDelete = '.tr' + $('#delete').val();
+			var userLogin= $(idDelete).find('.login').val();
+			if ( $(idDelete).length > 0){
+				var conf = confirm('Удалить пользователя' +' ' + userLogin + '?');
+				if(!conf) return false;
+				$(idDelete).find('td').css('background', '#f44242');			
+				$(idDelete).find('td').animate({
+						'opacity' : '0'
+					}, 800)
+
+				setTimeout(function (){
+					$(idDelete).remove();
+				}, 800)
+				current ++;
+					$('<div></div>').addClass('message-item').appendTo(message)
+					$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+					+ '<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() +
+					'</span>' + deleteMessage + '<b>' + userLogin + '</b>')		
+			}
+			else if($('#delete').val() == ''){
+				$('#error_no_user').animate({
+					opacity : 'show',
+				})
+				$('<div></div>').addClass('message-item').appendTo(message)
+				$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+					+'<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() + '</span>'
+					+ errorMessage2);
+			}
+			else{
+				$('#error_no_id').animate({
+					opacity : 'show',
+				})
+				$('<div></div>').addClass('message-item').appendTo(message)
+				$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+					+'<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() + '</span>'
+					+ errorMessage2);
+			}
+		});
+
+
+		//Анимация таблицы
+		$('#show_user_button').on('click', function(event){
+			 event.preventDefault();
+			$("#table_users_box input").attr("disabled", "");
+			if($(table).is(':hidden')){
+				$(table).slideDown(300);
+				$(this).removeClass('open-button').addClass('close-button').text('Скрыть всех пользователей');;
+			}else{
+				$(table).slideUp(300)
+				$(this).removeClass('close-button').addClass('open-button').text('Показать всех пользователей');
+			}
+		});
+
+
+		//Удаление пользователя из таблицы
+		$(document).on('click', '.del',function(){
+			var date = new Date();
+			var userLogin = $(this).parents('tr').find('.login').val();
+			var conf = confirm('Удалить пользователя' +' ' + userLogin+ '?');
+			if(!conf) return false;
+			var idDelete = $(this).parent().parent();
+			$(idDelete).find('td').css('background', '#f44242');			
+			$(idDelete).find('td').animate({
+					'opacity' : '0'
+				}, 800);
+
+			setTimeout(function(){
+				$(idDelete).remove();
+			}, 800)
+			current ++;
+			$('<div></div>').addClass('message-item').appendTo(message)
+				$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+				+ '<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() +
+				'</span>' + deleteMessage + '<b>' + userLogin + '</b>')	
+		});
+
+
+		//редактирование пользователей
+		$(document).on('click', '.edit',function(){
+			current ++;
+			$(this).parents('tr').find('input').removeAttr("disabled").end()
+								 .children('td').not(':first-child, :last-child, .editDate').addClass('active_edit');
+			$('#info-box').find('span').hide()				 
+			$('#edit_user').animate({
+					opacity : 'show',
+				})				 
+		});
+
+
+		//сохранение информации пользователя
+		$(document).on('click', '#save',function() {
+			current ++;
+			$('#table_users input').attr('disabled', '');
+			$('#table_users tr td').removeClass('active_edit');
+			$('#info-box').find('span').hide()				 
+			$('#save_user').animate({
+					opacity : 'show',
+				})	
+			var date = new Date()
+			$('<div></div>').addClass('message-item').appendTo(message)
+			$('.message-item:last').append('<span class="cur-message">' + current + '</span>' 
+				+'<span class="time">'+ date.getHours()+':'+date.getMinutes()+':'+date.getSeconds() + '</span>'
+				+ editMessage);
+		});
+
+
+		//Анимация блока сообщений
+		$('#mess_user_button').on('click', function(event){
+			event.preventDefault();
+			if($(message).is(':hidden')){
+				$(message).slideDown(500);
+				$(this).removeClass('open-button').addClass('close-button').text('Скрыть события');
+			}
+			else{
+				$(message).slideUp(500);
+				$(this).removeClass('close-button').addClass('open-button').text('Показать события');
+			}
+		});
+		
+
+		//Очистка сообщений
+		$('#clear_message_button').on('click', function(){
+			$('.message-item').css('background', '#f44242').animate({'opacity' : '0'});
+			setTimeout(function(){
+				$('.message-item').remove()
+			}, 800)				
+		});
+
+	
+		//Убрать всплывающие ошибки в полях
+		$('#error_no_id , #error_no_user, #error_login').hover(function(){
+			$(this).animate({
+				opacity : 'hide'
+			});
+		});
+
+
+		//Визуальное оформление таблицы
+		$('#table_users tr td:last-child').css('padding', '10px');
+		$('#table_users tr').eq(1).css({
+			'padding': '4px 0',
+			'background': 'rgb(196, 196, 196)',
+		});
+		$('#table_users tr td:first-child').css('width', '50px')
+
+	
+	});	
